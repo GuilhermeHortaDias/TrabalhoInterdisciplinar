3dc38cee8759dd3738ef219b5e67ca9ea769e3d4
Lucas Duarte <lucasbatistasuarte@gmail.com>
2025-06-26 20:52:00 -0300

    crud realizado

--- cadastro_local.cpp
+++ cadastro_local.cpp
@@ -16,12 +16,25 @@ public:
         this->x = x;
         this->y = y;
     }
+
+    void atualizar()
+    {
+        
+        cout << "Corrija o nome: ";
+        cin >> this->nome;
+        cout << "Corrija a coordenada X: ";
+        cin >> this->x;
+        cout << "Corrija a coordenada Y: ";
+        cin >> this->y;
+        cout << "Correção Realizda com sucesso." << endl;
+
+    }
 };
 
+vector<Local> locais;
 void criar_local(void)
 {
     int quantidade = 0;
-    vector<Local> locais;
 
     while (true)
     {
@@ -63,4 +76,43 @@ void criar_local(void)
         quantidade++;
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
+}
+
+void listar_locais()
+{
+    for (int i = 0; i < locais.size(); i++) // itera por todos os locais no vetor local e printa todos para o usuario.
+    {
+        cout << "Local: " << locais[i]. nome << endl;
+        cout << "Coordenadas: " << locais[i].x << " , " << locais[i].y << endl;
+    }
+}
+
+void deletar_local(string local)
+{
+    try
+        {
+            for (int i = 0; i < locais.size(); i++)
+            {
+                if (local == locais[i].nome)
+                {
+                    locais.erase(locais.begin() + i);
+                    cout << "Removido com sucesso" << endl;
+                    return;
+                }
+            }
+            throw 404;
+        }
+    catch (int error)
+    {
+        cout << "Nao foi possivel encontrar a conta para remover" << endl;
+    }
+}
+
+
+int main(void)
+{
+    criar_local();
+    listar_locais();
+    locais[0].atualizar();
+    deletar_local("teste");
 }
