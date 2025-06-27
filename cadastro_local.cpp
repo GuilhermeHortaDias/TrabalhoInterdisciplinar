648cc04565b0c3d4726094a5fb7fdf60c54e0a8d
Lucas Duarte <lucasbatistasuarte@gmail.com>
2025-06-26 21:46:57 -0300

    Correção orientação a objetos.

--- cadastro_local.cpp
+++ cadastro_local.cpp
@@ -6,10 +6,11 @@ using namespace std;
 
 class Local
 {
-public:
+private:
     string nome;
     float x, y;
 
+public:
     Local(string nome, float x, float y)
     {
         this->nome = nome;
@@ -17,102 +18,130 @@ public:
         this->y = y;
     }
 
-    void atualizar()
-    {
-        
-        cout << "Corrija o nome: ";
-        cin >> this->nome;
-        cout << "Corrija a coordenada X: ";
-        cin >> this->x;
-        cout << "Corrija a coordenada Y: ";
-        cin >> this->y;
-        cout << "Correção Realizda com sucesso." << endl;
 
+    // getters
+    string get_nome()
+    {
+        return nome;
+    }
+    float get_x()
+    {
+        return x;
+    }
+        float get_y()
+    {
+        return y;
+    }
+
+
+    void change_nome(string nome)
+    {
+        this->nome = nome;
+    }
+    void change_x(float x)
+    {
+        this->x = x;
+    }
+    void change_y(float y)
+    {
+        this->y = y;
     }
 };
 
-vector<Local> locais;
-void criar_local(void)
+
+
+class GerenciadorLocais
 {
-    int quantidade = 0;
-
-    while (true)
+    public:
+    vector<Local> locais;
+    
+    GerenciadorLocais()
     {
-        string nome;
-        float x, y;
-        bool existe = false;
+        vector<Local> locais;
+    }
 
-        cout << "Digite o nome do local: ";
-        getline(cin, nome);
-        if (nome == "")
-        {
-            break;
-        }
 
-        if (quantidade > 0)
+        void deletar_local(string local)
         {
-            for (int i = 0; i < quantidade; i++)
-            {
-                if (locais[i].nome == nome)
+            try
                 {
-                    cout << "Ja existe esse local" << endl;
-                    existe = true;
-                    break; // sai do loop que checa se existe nome igual
+                    for (int i = 0; i < locais.size(); i++)
+                    {
+                        if (local == locais[i].get_nome())
+                        {
+                            locais.erase(locais.begin() + i);
+                            cout << "Removido com sucesso" << endl;
+                            return;
+                        }
+                    }
+                    throw 404;
                 }
+            catch (int error)
+            {
+                cout << "Nao foi possivel encontrar a conta para remover" << endl;
             }
         }
 
-        if (existe == true)
+
+        void criar_local(string nome, float x, float y)
         {
-            continue; // se ja exisir um lugar com o nome, vai ir para a proxima iteração
+          Local local(nome, x, y);
+          locais.push_back(local);
+          cout << "Local criado com sucesso" << endl;
         }
 
-        cout << "Digite o valor x da coordenada: ";
-        cin >> x;
-        cout << "Digite o valor y da coordenada: ";
-        cin >> y;
-        Local novo_local(nome, x, y);
-        locais.push_back(novo_local);
-        quantidade++;
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    }
-}
 
-void listar_locais()
-{
-    for (int i = 0; i < locais.size(); i++) // itera por todos os locais no vetor local e printa todos para o usuario.
-    {
-        cout << "Local: " << locais[i]. nome << endl;
-        cout << "Coordenadas: " << locais[i].x << " , " << locais[i].y << endl;
-    }
-}
-
-void deletar_local(string local)
-{
-    try
+        void listar_locais(void)
         {
-            for (int i = 0; i < locais.size(); i++)
+            for (int i = 0; i < locais.size(); i++) // itera por todos os locais no vetor local e printa todos para o usuario.
             {
-                if (local == locais[i].nome)
+                cout << "Local: " << locais[i].get_nome() << endl;
+                cout << "Coordenadas: " << locais[i].get_x() << " , " << locais[i].get_y() << endl;
+            }
+        }
+
+        void atualizar(string nome_atual, string novo_nome, float x, float y)
+        {
+            try
                 {
-                    locais.erase(locais.begin() + i);
-                    cout << "Removido com sucesso" << endl;
-                    return;
+                    for (int i = 0; i < locais.size(); i++)
+                    {
+                        if (nome_atual == locais[i].get_nome() && nome_atual != novo_nome)
+                        {
+                            locais[i].change_nome(novo_nome);
+                            locais[i].change_x(x);
+                            locais[i].change_y(y);
+                            return;
+                        }
+                        else if (nome_atual != novo_nome)
+                        {
+                            throw 505;
+                        }
+                    }
+                    throw 404;
+                }
+            catch (int num)
+            {
+                if (num == 404)
+                    {
+                        cout << "Nao foi possivel encontrar o local" << endl;
+                    }
+                else
+                {
+                    cout << "Esse local ja existe" << endl;
                 }
             }
-            throw 404;
+        
+
         }
-    catch (int error)
-    {
-        cout << "Nao foi possivel encontrar a conta para remover" << endl;
-    }
-}
+};
+
 
 
 int main(void)
 {
-    criar_local();
-    listar_locais();
-    locais[0].atualizar();
-    deletar_local("teste");
+    GerenciadorLocais gerenciador;
+    gerenciador.criar_local("teste", 10.3, 9.0);
+    gerenciador.listar_locais();
+
 }
